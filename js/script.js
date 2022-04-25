957472e71dae50ff45564e8b01e58c4cf637b736
Your Name <eps.digitlamarketing@gmail.com>
2022-04-25 12:34:19 +0600

    CookiePolicy Popup Create

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/***Cookie policy Home Popup Script start *****/
+window.addEventListener("load",function(){
+setTimeout(
+    function open(event){
+        document.querySelector(".popup").style.
+        display="block";
+    },
+    1000
+)
+});
+
+document.querySelector("#close").addEventListener
+("click", function(){
+    document.querySelector(".popup").style.display=
+    "none";
+});/***Cookie policy Home Popup Script end *****/
