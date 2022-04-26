b74d54deb6e807a594f63e77039cefd2abaa26ab
Your Name <eps.digitlamarketing@gmail.com>
2022-04-26 15:18:54 +0600

    Slider with hover button

--- js/script.js
+++ js/script.js
@@ -15,38 +15,21 @@ document.querySelector("#close").addEventListener
     "none";
 });/***Cookie policy Home Popup Script end *****/
 
-var $slider = $('.slider'); 
-var $slide = 'li'; 
-var $transition_time = 1000; 
-var $time_between_slides = 4000; 
-function slides(){ 
-  return $slider.find($slide);
+
+
+function service(evt, service) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(service).style.display = "block";
+  evt.currentTarget.className += " active";
+
+
 }
 
-slides().fadeOut();
-slides().first().addClass('active');
-slides().first().fadeIn($transition_time);
-
-$function = function(){
-      var $i = $slider.find($slide + '.active').index();
-
-      slides().eq($i).removeClass('active');
-      slides().eq($i).fadeOut($transition_time);
-
-      if (slides().length == $i + 1) $i = -1; // loop to start
-
-      slides().eq($i + 1).fadeIn($transition_time);
-      slides().eq($i + 1).addClass('active');
-    }
-
-$interval = setInterval($function, $transition_time +  $time_between_slides );
-
-$("img").on("mouseover",function(){
- clearInterval($interval);
-});
-
-$("img").on("mouseout",function(){
-  $interval = setInterval($function, $transition_time +  $time_between_slides );
-});
-
- 
