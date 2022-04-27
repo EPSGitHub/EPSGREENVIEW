db1b165bc8d47721a4c958e7d4ad617c96af19da
Your Name <eps.digitlamarketing@gmail.com>
2022-04-27 15:34:49 +0600

    setTime & Imgeout

--- js/script.js
+++ js/script.js
@@ -15,21 +15,5 @@ document.querySelector("#close").addEventListener
     "none";
 });/***Cookie policy Home Popup Script end *****/
 
-/*** for hover button *****/
 
-function service(evt, service) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(service).style.display = "block";
-  evt.currentTarget.className += " active";
-
-
-}
 
